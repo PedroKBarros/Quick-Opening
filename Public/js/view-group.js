24e8c03d100accbb32fe52630677fb7d526abfb3
Pedro Barros <pedrokatsbarros@gmail.com>
2023-02-14 01:19:24 -0300

    Added refresh page after adding link, limit displayed characters per link, and open all links functionality

--- Public/js/view-group.js
+++ Public/js/view-group.js
@@ -1,3 +1,5 @@
+const maxLabelLink = 40
+
 function loadGroupPage(){
     let groupName = getGroupNameByUrl()
     let mainContainer = document.getElementById("main-container")
@@ -28,7 +30,8 @@ function loadGroupPage(){
             '<div class="row d-flex align-items-left text-left">' +
                 '<div class="col-12 d-flex justify-content-left text-left">' +
                     '<h4 class="display-9 mx-3 mt-2"> LINKS </h4>' +
-                    '<button type="button" class="btn btn-outline-primary mx-3 mt-2">Open All</button>' +
+                    '<button type="button" class="btn btn-outline-primary mx-3 mt-2" disabled>Open All</button>' +
+                    '<button type="button" class="btn btn-outline-primary mt-2" disabled>Open Selected</button>' +
                 '</div>' +
             '</div>' +
             '<div class="row d-flex align-items-left text-left">' +
@@ -41,7 +44,8 @@ function loadGroupPage(){
         '<div class="row d-flex align-items-left text-left">' +
           '<div class="col-12 d-flex justify-content-left text-left mb-3">' +
               '<h4 class="display-9 mx-3 mt-2"> LINKS </h4>' +
-              '<button type="button" class="btn btn-outline-primary mx-3 mt-2">Open All</button>' +
+              '<button type="button" class="btn btn-outline-primary mx-3 mt-2" onClick="openAllLinks()">Open All</button>' +
+              '<button type="button" class="btn btn-outline-primary mt-2">Open Selected</button>' +
             '</div>' +
           '</div>' +
             getLinksShowHtml(groupUrls) +
@@ -53,15 +57,21 @@ function loadGroupPage(){
 
   function getLinksShowHtml(groupUrls){
     let linksHtml = ""
+    let linkLabel = ""
 
     linksHtml+= 
       '<div class="row d-flex align-items-left text-left mx-4 my-4">' +
           '<ul class=list-group>'
     for(let i = 0;i < groupUrls.length;i++){
+      if (groupUrls[i].length <= maxLabelLink)
+        linkLabel = groupUrls[i]
+      else
+        linkLabel = groupUrls[i].substring(0, maxLabelLink + 1) + "..."
+
       linksHtml+= 
           '<li class="list-group-item">' +
             '<input class="form-check-input me-1" type="checkbox" value="" aria-label="...">' +
-              '<a href="' + group.urls[i] + '" class="list-group-item list-group-item-action link-primary" target="_blank">' + groupUrls[i] + '</a>' +
+              '<a href="' + group.urls[i] + '" class="list-group-item list-group-item-action link-primary" target="_blank">' + linkLabel + '</a>' +
             '</li>'
     }
     linksHtml +=
@@ -95,8 +105,18 @@ function loadGroupPage(){
     let index = getIndexGroup(groupName)
     groups[index].urls.push(inputLink.value)
     window.localStorage.setItem("qoGroups", JSON.stringify(groups))
+    window.location.reload()
     return true
 
+  }
+
+  function openAllLinks(){
+    let groupName = getGroupNameByUrl()
+    links = getJSONGroups()[getIndexGroup(groupName)].urls
+
+    for(i = 0;i < links.length;i++){
+      window.open(links[i], '_blank')
+    }
   }
 
   function isValidUrl(urlString){
